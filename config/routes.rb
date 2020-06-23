27d9ba6c3b24d89a9a55234090b2bfea76b79cc5
Lorenna Vieira <lorennasv@gmail.com>
2020-06-23 14:25:40 +0100

    changed route to landingpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'offices#index'
+  root to: 'pages#home'
   resources :offices, only: %i[index show new create] do
     resources :bookings, only: %i[new create edit update]
   end
