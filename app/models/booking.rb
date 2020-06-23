e856ab3b1ccb2f1a7cce1771812e13172a2ef780
DariusPirvulescu <organicdarius@gmail.com>
2020-06-23 20:46:15 +0300

    added inclusion to booking's category (rent, visit)

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :office
   belongs_to :user
 
+  validates :category, inclusion: { in: %w(rent visit) }
   validates :start_date, presence: true
   validates :end_date, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,39 +23,24 @@ puts "creating users"
   User.create(name: Faker::Name.name, email: Faker::Internet.email , password: "123456")
 end
 
-puts "creating offices"
-office_names = ["Cowork", "Office", "Space"]
-addresses = ["Lisbon", "Porto", "Faro", "Vila Real", "Viseu"]
-20.times do
-  office_address = addresses.sample
-  office_name = "#{office_names.sample} in #{office_address}"
-  Office.create(name: office_name, address: office_address, description: Faker::Lorem.sentence(word_count: 10), price: rand(100..10000), user: User.all.sample)
-end
+# puts "creating offices"
+# office_names = ["Cowork", "Office", "Space"]
+# addresses = ["Lisbon", "Porto", "Faro", "Vila Real", "Viseu"]
+# 20.times do
+#   office_address = addresses.sample
+#   office_name = "#{office_names.sample} in #{office_address}"
+#   Office.create(name: office_name, address: office_address, description: Faker::Lorem.sentence(word_count: 10), price: rand(100..10000), user: User.all.sample)
+# end
 
-category = ["visit", "rent"]
-puts "creating bookings"
-100.times do
-  office = Office.sample.all
-  user = User.all.reject { |u| u == office.owner }
-  start_date = Time.now + rand(1..10)
-  duration = rand(1..30)
-  end_date = start_date + duration
-  Booking.create()
-end
+# category = ["visit", "rent"]
+# puts "creating bookings"
+# 100.times do
+#   office = Office.sample.all
+#   user = User.all.reject { |u| u == office.owner }
+#   start_date = Time.now + rand(1..10)
+#   duration = rand(1..30)
+#   end_date = start_date + duration
 
-# # Bookings
-#  t.datetime "start_date"
-#  t.datetime "end_date"
-#  t.bigint "office_id", null: false
-#  t.bigint "user_id"
-#  t.string "category"
-
-
-# office = Office.sample.all
-# User.all.reject { |u| u == office.owner }
-# start_date = Time
-# duration = rand(1..30)
-# end_date = start_date + duration
-# # user
-# offices
-# bookings
+#   Booking.create(start_date: start_date, end_date: end_date, office: office, user: user.sample, category: category.sample)
+# end
+puts "finished"
