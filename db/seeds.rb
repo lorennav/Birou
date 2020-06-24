83922ce2a2855fb2304fdea924159161af02ebb6
DariusPirvulescu <organicdarius@gmail.com>
2020-06-24 13:28:21 +0300

    seeded the db with users, offices, bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,11 @@ require "faker"
 #   url: Faker::Internet.url, votes: rand(0..50))
 #   post.save
 # end
-puts "creating users"
-100.times do
-  User.create(name: Faker::Name.name, email: Faker::Internet.email , password: "123456")
-end
+
+# puts "creating users"
+# 100.times do
+#   User.create(name: Faker::Name.name, email: Faker::Internet.email , password: "123456")
+# end
 
 # puts "creating offices"
 # office_names = ["Cowork", "Office", "Space"]
@@ -29,18 +30,23 @@ end
 # 20.times do
 #   office_address = addresses.sample
 #   office_name = "#{office_names.sample} in #{office_address}"
-#   Office.create(name: office_name, address: office_address, description: Faker::Lorem.sentence(word_count: 10), price: rand(100..10000), user: User.all.sample)
+
+#   office = Office.new(name: office_name, address: office_address, description: Faker::Lorem.sentence(word_count: 10), price: rand(100..10000), owner: User.all.sample)
+#   puts "creating #{office.name}"
+#   office.save
 # end
 
 # category = ["visit", "rent"]
 # puts "creating bookings"
 # 100.times do
-#   office = Office.sample.all
+#   office = Office.all.sample
 #   user = User.all.reject { |u| u == office.owner }
 #   start_date = Time.now + rand(1..10)
 #   duration = rand(1..30)
 #   end_date = start_date + duration
 
-#   Booking.create(start_date: start_date, end_date: end_date, office: office, user: user.sample, category: category.sample)
+#   booking = Booking.new(start_date: start_date, end_date: end_date, office: office, user: user.sample, category: category.sample)
+#   puts "#{booking.user.name} has booked #{booking.office.name}"
+#   booking.save
 # end
-puts "finished"
+# puts "finished"
