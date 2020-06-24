a5208850591aafbe13e15cda67d26299b7d900f7
DariusPirvulescu <organicdarius@gmail.com>
2020-06-24 14:40:20 +0300

    authorised general access to office's show and index. Implemented cloudinary. Seeded the Db with offices photos

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -1,4 +1,5 @@
 class OfficesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :index]
   def index
     if params[:search].nil? || params[:search] == ""
       @offices = Office.all
--- app/models/office.rb
+++ app/models/office.rb
@@ -2,6 +2,7 @@ class Office < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings
   has_many :users, through: :bookings
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
