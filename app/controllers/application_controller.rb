d8ece19c89d26f2b2899b4786833f61a7474e591
DariusPirvulescu <organicdarius@gmail.com>
2020-06-25 19:18:05 +0300

    changed form in office show page logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,30 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+  before_action :store_user_location!, if: :storable_location?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def configure_permitted_parameters
+  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    # return the path based on resource
+    stored_location_for(resource)
+  end
+
+  private
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
   end
 end
