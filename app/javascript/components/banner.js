3c8c0917420edd682f5c8868d914e45cf9412c71
carlos madeira <cfmadeira@gmail.com>
2020-06-26 11:23:51 +0100

    typed loop_false in bookingindex

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,8 +3,8 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Here are all your bookings"],
-    typeSpeed: 75,
-    loop: true
+    typeSpeed: 85,
+    loop: false
   });
 }
 
